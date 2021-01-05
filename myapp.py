0fcfdbdca109b33f5933db55fdaf7af18bfa9ea4
Charles Gao <gao440@purdue.com>
2021-01-04 21:22:48 -0500

    trying changes

--- myapp.py
+++ myapp.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import pandas as pd
-import io
+#import iopyth
 import base64
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
